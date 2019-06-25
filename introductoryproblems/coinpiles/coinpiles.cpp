3a3d64c888698cd058a237c36ebef2a3e7545b17
reeceyang <reeceyang@live.com>
2019-06-24 22:15:51 -0500

    Finish coinpiles

---
+++ introductoryproblems/coinpiles/coinpiles.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int a,b;
+        cin >> a >> b;
+        if ((a + b) % 3 == 0 && 2 * a >= b && 2 * b >= a) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+}
