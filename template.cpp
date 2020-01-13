f60136564d6ec2ce633961c5d096bc27ffa4f341
Reece Yang <reeceyang@live.com>
2020-01-13 11:09:49 -0500

    Add some macros

--- template.cpp
+++ template.cpp
@@ -6,15 +6,22 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef vector<long long> vll;
 typedef pair<int,int> pi;
+typedef vector<pi> vpi;
 
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
 #define REP(i,a,b) for (int i = a; i <= b; i++)
+#define TRAV(a, x) for (auto& a : x)
+
+void setIO(string name) {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((name+".in").c_str(),"r",stdin);
+    freopen((name+".out").c_str(),"w",stdout);
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    //setIO("name");
 
 }
