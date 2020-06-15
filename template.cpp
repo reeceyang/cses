f08ce04985d1729f2b89c893d8014b57c04891ce
reeceyang <reeceyang@live.com>
2020-06-15 12:18:54 -0500

    update template with no-argument setIO function

--- template.cpp
+++ template.cpp
@@ -21,7 +21,11 @@ void setIO(string name) {
     freopen((name+".out").c_str(),"w",stdout);
 }
 
+void setIO() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+}
+
 int main() {
-    //setIO("name");
+    setIO();
 
 }
